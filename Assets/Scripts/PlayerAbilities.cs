2042b0fb2159e3b333988c3d9df42d283f674fa9
MaybeCanadian <1coffeye@gmail.com>
2022-11-03 10:27:19 -0400

    added abilities to the battle scene. Player can use abilities with buttons and they play an animation.

--- Assets/Scripts/PlayerAbilities.cs
+++ Assets/Scripts/PlayerAbilities.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerAbilities : MonoBehaviour
 {
-    public Button ability1;
-    public Button ability2;
-    public Button ability3;
-    public Button ability4;
-
     public Animator anim;
+
+    public TMP_Text button1;
+    public TMP_Text button2;
+    public TMP_Text button3;
+    public TMP_Text button4;
+
+    public string Ability1Name;
+    public string Ability2Name;
+    public string Ability3Name;
+    public string Ability4Name;
+
+    private void Start()
+    {
+        button1.text = Ability1Name;
+        button2.text = Ability2Name;
+        button3.text = Ability3Name;
+        button4.text = Ability4Name;
+    }
 
     public void OnButton1Pressed()
     {
